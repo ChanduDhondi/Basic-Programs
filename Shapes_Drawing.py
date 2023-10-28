ff5484a809c4e305159b9484b3fd172730d9da3d
ChanduDhondi <chandudhondi23@gmail.com>
2023-10-28 22:51:04 +0530

    semi circle method added

--- Shapes_Drawing.py
+++ Shapes_Drawing.py
@@ -6,10 +6,16 @@ bg = turtle.bgcolor("light yellow")
 bg = turtle.title("Drawing Shapes")
 
 pen = turtle.Turtle()
+pen.hideturtle()
 
+pen.teleport(0,200)
+pen.write("Drawing Shapes",False,"center")
+pen.up()
+pen.home()
+pen.down()
+pen.speed(6)
 
-class Shape():
-
+class Shapes():
     def __init__(self,pen_clr,fl_clr):
          self.pen_clr = pen_clr
          self.fl_clr = fl_clr
@@ -22,15 +28,24 @@ class Shape():
          for _ in range(self.sides):  
             pen.forward(50)
             pen.right(360/self.sides)
+    def semi_circle(self):
+        pen.up()
+        pen.setpos(-100,100)
+        pen.down()
+        pen.color(self.pen_clr)
+        pen.begin_fill()
+        pen.fillcolor(self.fl_clr)
+        pen.circle(50,90)
+        pen.left(90)
+        pen.forward(100)
+        pen.setheading(270)
+        pen.circle(50,90)
+        pen.end_fill()
+          
+shape = Shapes('green','purple')
+shape.draw(4)
+shape.semi_circle()
 
-square = Shape('black','green')
-square.draw(8)
-square.draw(7)
-square.draw(6)
-square.draw(5)
-square.draw(4)
-square.draw(3)
-square.draw(2)
 
 turtle.done()
 
