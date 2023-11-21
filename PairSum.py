2e48f4b1d74dd68f795222d3f04c587214f64c9e
ChanduDhondi <chandudhondi23@gmail.com>
2023-11-21 22:13:37 +0530

    new logic added

--- PairSum.py
+++ PairSum.py
@@ -17,19 +17,32 @@ class PairSum:
     
     def logic2(self):
         # Time Complexity is O(N)
+        self.arr1 = []
         self.result = []
         for i in range(len(arr)):
-            self.num = arr.pop()
-            self.diff= self.s-self.num
-            if self.diff in self.arr:
-                self.result.append((self.diff,self.num))
+            self.diff= self.s-self.arr[i]
+            if self.diff in self.arr1:
+                self.result.append((arr[i],self.diff))                           
+            self.arr1.append(arr[i])
         return self.result
-                     
+    
+    def logic3(self):
+        # Using list.pop()
+        # Time complexity is O(N)
+        self.result = []
+        for i in range(len(self.arr)):
+            self.num = self.arr.pop()
+            self.diff = self.s - self.num
+            if self.diff in self.arr:
+                self.result.append((self.diff,self.num))            
+        return self.result 
+                
 if __name__ == '__main__':
-    arr = [1,5,2,7,11,4,9]
-    s = 9
+    arr = [1,3,4,3,2,5]
+    s = 6
     pairsum = PairSum(arr,s)
     print(pairsum.logic1())
     print(pairsum.logic2())
+    print(pairsum.logic3())
 
   
